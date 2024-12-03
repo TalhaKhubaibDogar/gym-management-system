93729543f1d5ae95be0cc35ad6555c717c42c254
TalhaKhubaibDogar <talhakhubaib56@gmail.com>
2024-12-03 22:52:29 +0500

    Update Design Of SignUp page

--- client/src/app/signup/page.js
+++ client/src/app/signup/page.js
@@ -1,64 +1,29 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import MenuItem from '@mui/material/MenuItem';
+import { FaLock } from 'react-icons/fa'; // React Icon for lock
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import axios from 'axios';
-import CandyModal from '../components/CandyModal';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-const defaultTheme = createTheme();
+// Import the SCSS file
+import styles from './SignUp.module.scss'; // Assuming the file is named SignUp.module.scss
+
+// Validation schema
 const validationSchema = Yup.object({
   firstName: Yup.string().required('First Name is required'),
   lastName: Yup.string().required('Last Name is required'),
-  email: Yup.string()
-    .email('Invalid email address')
-    .required('Email is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
   password: Yup.string().min(8).required('Password is required'),
-  reenterPassword: Yup.string()
-    .oneOf([Yup.ref('password'), null], 'Passwords must match')
-    .min(8)
-    .required('Re-enter Password is required'),
+  reenterPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').min(8).required('Re-enter Password is required'),
   userType: Yup.string().required('User Type is required'),
 });
 
 export default function SignUp() {
   const router = useRouter();
-  const [totalCandy, setTotalCandy] = useState(0);
-  const [candyOpen, setCandyOpen] = useState(false);
 
-  useEffect(() => {
-    try {
-      axios.get('/api/CandyPayments').then((res) => {
-        setTotalCandy(res?.data?.totalDonations / 100);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
-  const handleSubmit = (values, { setSubmitting }) => {
-    if (values.userType === '2') {
-      handleSubmitAPI(values, { setSubmitting });
-    } else {
-      localStorage.setItem('signupData', JSON.stringify(values));
-      setCandyOpen(true);
-      setSubmitting(false);
-    }
-  };
-
-  const handleSubmitAPI = async (values, { setSubmitting }) => {
+  const handleSubmit = async (values, { setSubmitting }) => {
     const signupData = {
       email: values.email,
       first_name: values.firstName,
@@ -70,13 +35,11 @@ export default function SignUp() {
 
     try {
       const response = await axios.post('https://api.candypaint.us/api/v1/users/signup/', signupData);
-      // Handle successful signup, e.g., navigate to the verification page or login page
       console.log(response.data);
       localStorage.setItem('email', values.email);
       router.push('/verify');
     } catch (error) {
       console.error('Signup error:', error);
-      // Handle signup error, e.g., show an error message
       alert(error.response.data.data.message);
     } finally {
       setSubmitting(false);
@@ -84,211 +47,119 @@ export default function SignUp() {
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <div className={styles.avatar}>
+          <FaLock size={50} color="#fff" />
+        </div>
+        <h1>Sign Up</h1>
+        <Formik
+          initialValues={{
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            reenterPassword: '',
+            userType: '',
           }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Sign up
-          </Typography>
-          <Formik
-            initialValues={{
-              firstName: '',
-              lastName: '',
-              email: '',
-              password: '',
-              reenterPassword: '',
-              userType: '',
-            }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({
-              isSubmitting,
-              touched,
-              setFieldTouched,
-              setFieldValue,
-              errors,
-            }) => (
-              <Form noValidate>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      as={TextField}
-                      autoComplete='given-name'
-                      name='firstName'
-                      required
-                      fullWidth
-                      id='firstName'
-                      label='First Name'
-                      onChange={(e) => {
-                        setFieldValue('firstName', e.target.value);
-                        setFieldTouched('firstName', true, false);
-                      }}
-                      helperText={
-                        touched.firstName && errors.firstName ? (
-                          <ErrorMessage name='firstName' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(touched.firstName && errors.firstName)}
-                    />
-                  </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <Field
-                      as={TextField}
-                      required
-                      fullWidth
-                      id='lastName'
-                      label='Last Name'
-                      name='lastName'
-                      autoComplete='family-name'
-                      onChange={(e) => {
-                        setFieldValue('lastName', e.target.value);
-                        setFieldTouched('lastName', true, false);
-                      }}
-                      helperText={
-                        touched.lastName && errors.lastName ? (
-                          <ErrorMessage name='lastName' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(touched.lastName && errors.lastName)}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Field
-                      as={TextField}
-                      required
-                      fullWidth
-                      id='email'
-                      label='Email Address'
-                      name='email'
-                      autoComplete='email'
-                      onChange={(e) => {
-                        setFieldValue('email', e.target.value);
-                        setFieldTouched('email', true, false);
-                      }}
-                      helperText={
-                        touched.email && errors.email ? (
-                          <ErrorMessage name='email' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(touched.email && errors.email)}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Field
-                      as={TextField}
-                      required
-                      fullWidth
-                      name='password'
-                      label='Password'
-                      type='password'
-                      id='password'
-                      autoComplete='new-password'
-                      onChange={(e) => {
-                        setFieldValue('password', e.target.value);
-                        setFieldTouched('password', true, false);
-                      }}
-                      helperText={
-                        touched.password && errors.password ? (
-                          <ErrorMessage name='password' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(touched.password && errors.password)}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Field
-                      as={TextField}
-                      required
-                      fullWidth
-                      name='reenterPassword'
-                      label='Re-enter Password'
-                      type='password'
-                      id='reenterPassword'
-                      autoComplete='new-password'
-                      onChange={(e) => {
-                        setFieldValue('reenterPassword', e.target.value);
-                        setFieldTouched('reenterPassword', true, false);
-                      }}
-                      helperText={
-                        touched.reenterPassword && errors.reenterPassword ? (
-                          <ErrorMessage name='reenterPassword' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(
-                        touched.reenterPassword && errors.reenterPassword
-                      )}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      select
-                      required
-                      fullWidth
-                      id='userType'
-                      label='Select User Type'
-                      name='userType'
-                      onChange={(e) => {
-                        setFieldValue('userType', e.target.value);
-                        setFieldTouched('userType', true, false);
-                      }}
-                      helperText={
-                        touched.userType && errors.userType ? (
-                          <ErrorMessage name='userType' />
-                        ) : (
-                          ''
-                        )
-                      }
-                      error={Boolean(touched.userType && errors.userType)}
-                    >
-                      <MenuItem value='2'>User</MenuItem>
-                      <MenuItem value='1'>Hotspot</MenuItem>
-                    </TextField>
-                  </Grid>
-                </Grid>
-                <Button
-                  type='submit'
-                  fullWidth
-                  variant='contained'
-                  sx={{ mt: 3, mb: 2 }}
-                  style={{ backgroundColor: '#272727', color: '#fff' }}
-                >
-                  Sign Up
-                </Button>
-                <Grid container justifyContent='center'>
-                  <Grid item>
-                    <Link href='/login' legacyBehavior>
-                      <a>{'Already have an account? Sign in'}</a>
-                    </Link>
-                  </Grid>
-                </Grid>
-              </Form>
-            )}
-          </Formik>
-        </Box>
-      </Container>
-      <CandyModal open={candyOpen} setOpen={setCandyOpen} />
-    </ThemeProvider>
+          {({ isSubmitting, touched, setFieldTouched, setFieldValue, errors }) => (
+            <Form>
+              <div className={styles.formGroup}>
+                <label htmlFor="firstName">First Name</label>
+                <Field
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  onChange={(e) => {
+                    setFieldValue('firstName', e.target.value);
+                    setFieldTouched('firstName', true, false);
+                  }}
+                />
+                {touched.firstName && errors.firstName && <div className={styles.error}>{errors.firstName}</div>}
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="lastName">Last Name</label>
+                <Field
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  onChange={(e) => {
+                    setFieldValue('lastName', e.target.value);
+                    setFieldTouched('lastName', true, false);
+                  }}
+                />
+                {touched.lastName && errors.lastName && <div className={styles.error}>{errors.lastName}</div>}
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="email">Email Address</label>
+                <Field
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={(e) => {
+                    setFieldValue('email', e.target.value);
+                    setFieldTouched('email', true, false);
+                  }}
+                />
+                {touched.email && errors.email && <div className={styles.error}>{errors.email}</div>}
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="password">Password</label>
+                <Field
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={(e) => {
+                    setFieldValue('password', e.target.value);
+                    setFieldTouched('password', true, false);
+                  }}
+                />
+                {touched.password && errors.password && <div className={styles.error}>{errors.password}</div>}
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="reenterPassword">Re-enter Password</label>
+                <Field
+                  type="password"
+                  name="reenterPassword"
+                  id="reenterPassword"
+                  onChange={(e) => {
+                    setFieldValue('reenterPassword', e.target.value);
+                    setFieldTouched('reenterPassword', true, false);
+                  }}
+                />
+                {touched.reenterPassword && errors.reenterPassword && <div className={styles.error}>{errors.reenterPassword}</div>}
+              </div>
+
+              <div className={styles.formGroup}>
+                <label htmlFor="userType">User Type</label>
+                <Field as="select" name="userType" id="userType" onChange={(e) => {
+                  setFieldValue('userType', e.target.value);
+                  setFieldTouched('userType', true, false);
+                }}>
+                  <option value="">Select User Type</option>
+                  <option value="2">User</option>
+                  <option value="1">Hotspot</option>
+                </Field>
+                {touched.userType && errors.userType && <div className={styles.error}>{errors.userType}</div>}
+              </div>
+
+              <button className={styles.button1} type="submit" disabled={isSubmitting}>
+                Sign Up
+              </button>
+            </Form>
+          )}
+        </Formik>
+        <div className={styles.linkContainer}>
+          <Link href="/login">Already have an account? Sign in</Link>
+        </div>
+      </div>
+    </div>
   );
 }
